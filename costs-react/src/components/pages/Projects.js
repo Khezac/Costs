30a5cf9c432a653c8ab7b3443a2b595b856672ed
Khezac <khezac1@gmail.com>
2024-01-03 11:45:17 -0300

    fix.container-component-css-fix

--- costs-react/src/components/pages/Projects.js
+++ costs-react/src/components/pages/Projects.js
@@ -1,4 +1,6 @@
+import Container from '../layout/Container'
 import Message from '../layout/Message'
+import LinkButton from '../layout/LinkButton'
 import {useLocation} from 'react-router-dom'
 
 import styles from './Projects.module.css'
@@ -12,9 +14,15 @@ function Projects(){
     }
 
     return(
-        <div>
-            <h1>Meus Projetos</h1>
+        <div className={styles.project_container}>
+            <div className={styles.title_container}>
+                <h1>Meus Projetos</h1>
+                <LinkButton to="/newproject" text="Criar Projeto"  />
+            </div>
             {message && <Message type='success' msg={message} />}
+            <Container customClass='start'>
+                <p>projetos...</p>
+            </Container>
         </div>
     )
 }
